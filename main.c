8414bd01551d65080571c2f7e65abebdc248c5cc
Emanuele Canessa <ema0185@gmail.com>
2015-09-01 22:24:47 +0200

    main.c: removed \t

--- main.c
+++ main.c
@@ -7,15 +7,15 @@ Version     : 1.0
 */
 #include "Y4M2YUV.h"
 
-int main(int argc, char** argv)
+int
+main(int argc, char ** argv)
 {
-	if(argc != 3)
-	{
-		doHelp();
-	}
-	convertY4M(argv[1], argv[1]);
-	return(0);
+  if(argc != 3)
+  {
+    doHelp();
+  }
+
+  convertY4M(argv[1], argv[1]);
+
+  return 0;
 }
-
-
-
